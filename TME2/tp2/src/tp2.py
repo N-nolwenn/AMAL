e06cd73b9d9a022fe25e0cab4a413c083081c0ba
N-nolwenn <nolwenn.pigeon@gmail.com>
2023-11-02 16:43:09 +0900

    TME2

--- TME2/tp2/src/tp2.py
+++ TME2/tp2/src/tp2.py
@@ -6,6 +6,7 @@ import datamaestro
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 
+
 data = datamaestro.prepare_dataset("edu.uci.boston")
 colnames, datax, datay = data.data()
 
@@ -65,7 +66,7 @@ def linear_regression(xtrain, ytrain, xtest, ytest, batch_size = None, epsilon =
         ##  TODO:  Calcul du backward (grad_w, grad_b)
         train_loss.backward(retain_graph = True)
 
-        ##  TODO:  Mise à jour des paramètres du modèle
+        ##  Mise à jour des paramètres du modèle
         with torch.no_grad():
             w -= epsilon * w.grad
             b -= epsilon * b.grad
@@ -82,5 +83,80 @@ def linear_regression(xtrain, ytrain, xtest, ytest, batch_size = None, epsilon =
     test_writer.close()
 
 #linear_regression(xtrain, ytrain, xtest, ytest, batch_size=10)
-#print("stochastique", linear_regression(xtrain, ytrain, xtest, ytest, batch_size=1))
-#print("minibatch", linear_regression(xtrain, ytrain, xtest, ytest, batch_size=10))
+#linear_regression(xtrain, ytrain, xtest, ytest, batch_size=1)
+#plinear_regression(xtrain, ytrain, xtest, ytest, batch_size=10)
+
+NB_EPOCH = 1000
+
+def optimiseur(xtrain, ytrain, xtest, ytest, batch_size = None, epsilon = 1e-6):
+
+    optim_writer = SummaryWriter("logs_optim")
+    optim_test_writer = SummaryWriter("logs_optim_test")
+
+    # Initialisation de la loss
+    optim = torch.optim.SGD(params=[w,b] lr= epsilon)
+    optim.zero_grad()
+
+    num_samples = xtrain.shape[0]
+
+    for n_iter in range(NB_EPOCH):
+
+        if batch_size is None:
+            # Scenario du Batch Gradient descent
+            ## On utilise tout le jeu de données pr la màj des paramètres
+            xbatch, ybatch = xtrain, ytrain 
+        elif batch_size == 1:
+            # Scenario du Stochastic Gradient descent
+            ## On choisit un index aléatoire et seulement 1 échantillon est utilisé pr màj
+            index = torch.randint(0, num_samples,())
+            # fonction torch.randit(_,_,_) génère un nombre entier aléatoire.
+            ## troisème paramètre spécifie la forme du tenseur de sortie ici scalaire
+            xbatch, ybatch = xtrain[index], ytrain[index]
+        else:
+            # Scenario du Mini-Batch Gradient descent 
+            ## batch_size > 1 on divise le jeu de données en pls petits ensembles
+            index = torch.randperm(num_samples)[:batch_size]
+            xbatch, ybatch = xtrain[index], ytrain[index]
+
+        # Forward
+        optim_train_loss = torch.nn.MSELoss(torch.matmul(xbatch, w) + b, ybatch)
+
+        optim_writer.add_scalar(f"Loss/optim_train{batch_size}", optim_train_loss, n_iter)
+
+        # Backward
+        optim_train_loss.backward(retain_graph = True)
+
+        # Màj des param
+        optim.step()
+        optim.zero_grad()
+
+        test_loss = torch.nn.MSE(torch.matmul(xtest, w)+b, ytest)
+        optim_test_writer.add_scalar(f"Loss/optim_test{batch_size}", test_loss, n_iter)
+    
+    optim_writer.close()
+    optim_test_writer.close()
+
+#optimiseur(xtrain, ytrain, xtest, ytest, batch_size=None)
+#optimiseur(xtrain, ytrain, xtest, ytest, batch_size=1)
+#optimiseur(xtrain, ytrain, xtest, ytest, batch_size=10)
+
+
+def Reseau1():
+
+    w1 = torch.nn.Parameter((xtrain.shape[1], ytrain.shape[1]), requires_grad = True)
+    b1 = torch.nn.Parameter(ytrain.shape[1], requires_grad = True)
+    w2 = torch.nn.Parameter((xtrain.shape[1], ytrain.shape[1]), requires_grad = True)
+    b2 = torch.nn.Parameter(ytrain.shape[1], requires_grad = True)
+    
+    optim = torch.optim.SGD(params=[w1,b1, w2, b2] lr= epsilon)
+    optim.zero_grad()
+
+    tanh = torch.nn.Tanh()
+    mse = torch.nn.MSELoss()
+
+def Reseau2():
+    model = torch.nn.Sequential(
+            torch.nn.Linear(xtrain.shape[1], ytrain.shape[1])
+            torch.nn.Tanh()
+            torch.nn.Linear(ytrain.shape[1],1))
+
