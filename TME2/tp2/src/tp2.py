6385a70a1206a0a11fb7f4737fd2fec443f5c2c7
N-nolwenn <nolwenn.pigeon@gmail.com>
2023-11-02 11:29:36 +0900

    TME2

--- TME2/tp2/src/tp2.py
+++ TME2/tp2/src/tp2.py
@@ -17,10 +17,11 @@ datay = torch.tensor(datay,dtype=torch.float, requires_grad = True).reshape(-1,1
 # Découpage des données en ensembles train (80%) et test (20%)
 xtrain, xtest, ytrain, ytest = train_test_split(datax, datay, test_size = 0.2)
 
-def linear_regression (xtrain, ytrain, batch_size = None, epsilon = 1e-6):
+
+def linear_regression(xtrain, ytrain, xtest, ytest, batch_size = None, epsilon = 1e-6):
 
     writer = SummaryWriter("logs")
-
+    test_writer = SummaryWriter("logs_test")
     # Intialisation fonctions Linéaire et Loss
     
 
@@ -31,6 +32,7 @@ def linear_regression (xtrain, ytrain, batch_size = None, epsilon = 1e-6):
     num_samples = xtrain.shape[0]
 
     for n_iter in range(1000):
+
         if batch_size is None:
             # Scenario du Batch Gradient descent
             ## On utilise tout le jeu de données pr la màj des paramètres
@@ -38,7 +40,7 @@ def linear_regression (xtrain, ytrain, batch_size = None, epsilon = 1e-6):
         elif batch_size == 1:
             # Scenario du Stochastic Gradient descent
             ## On choisit un index aléatoire et seulement 1 échantillon est utilisé pr màj
-            index = torch.randit(0, num_samples,())
+            index = torch.randint(0, num_samples,())
             # fonction torch.randit(_,_,_) génère un nombre entier aléatoire.
             ## troisème paramètre spécifie la forme du tenseur de sortie ici scalaire
             xbatch, ybatch = xtrain[index], ytrain[index]
@@ -49,16 +51,16 @@ def linear_regression (xtrain, ytrain, batch_size = None, epsilon = 1e-6):
             xbatch, ybatch = xtrain[index], ytrain[index]
 
         # Phase Forward
-        yhat = torch.mm(xbatch, w) + b 
+        yhat = torch.matmul(xbatch, w) + b 
         mse = torch.nn.MSELoss()
         train_loss = mse(yhat, ybatch)
 
         # on peut visualiser avec
-        # tensorboard --logdir runs/
-        writer.add_scalar('Loss/train', train_loss, n_iter)
+        # tensorboard --logdir logs/
+        writer.add_scalar(f"Loss/train{batch_size}", train_loss, n_iter)
 
         # Sortie directe
-        print(f"Itérations {n_iter}: loss {train_loss}")
+        #print(f"Itérations {n_iter}: Training loss {train_loss}")
 
         ##  TODO:  Calcul du backward (grad_w, grad_b)
         train_loss.backward(retain_graph = True)
@@ -71,5 +73,14 @@ def linear_regression (xtrain, ytrain, batch_size = None, epsilon = 1e-6):
             w.grad.zero_()
             b.grad.zero_()
 
-linear_regression(xtest, ytest)
-        
+
+        test_loss = mse(torch.matmul(xtest, w)+b, ytest)
+        test_writer.add_scalar(f"Loss/test{batch_size}", test_loss, n_iter)
+        #print(f"Iterations {n_iter} | Testing loss: {test_loss}")
+
+    writer.close()
+    test_writer.close()
+
+#linear_regression(xtrain, ytrain, xtest, ytest, batch_size=10)
+#print("stochastique", linear_regression(xtrain, ytrain, xtest, ytest, batch_size=1))
+#print("minibatch", linear_regression(xtrain, ytrain, xtest, ytest, batch_size=10))
