9d76e2892ad6c24f9994f2ac5a8a1997960ee1f6
N-nolwenn <nolwenn.pigeon@gmail.com>
2023-11-10 17:51:08 +0900

    TME3

--- TME3/tp3/src/tp3.py
+++ TME3/tp3/src/tp3.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 import datetime
+import matplotlib.pyplot as plt
 
 # Téléchargement des données MNIST
 from datamaestro import prepare_dataset
@@ -30,21 +31,38 @@ writer.add_image(f'samples', images, 0)
 savepath = Path("model.pch")
 
 #  TODO: 
+
+# Utilisez la fonction torch.min et torch.max sur l'ensemble des images MNIST
+min_value = torch.min(torch.tensor(train_images))
+max_value = torch.max(torch.tensor(train_images))
+
+# Affichez les valeurs minimales et maximales
+print(f"Valeur minimale : {min_value.item()}")
+print(f"Valeur maximale : {max_value.item()}")
+
+
 class MonDataset(Dataset):
     def __init__(self, images, labels):
-        self.images = torch.tensor(images).unsqueeze(1).double()/255
+        # On convertit l'ensemble des images en tensor pytorch 
+        ## .unsqueeze(1) transforme le tensor d'images '(batch_size, height, width)' en '(batch_size, 1, height, width)'
+        ### les valeurs des pixels dans une image MNIST sont des entiers entre 0 et 255. DIviser par 255 permet de normaliser.
+        self.images = torch.tensor(images).unsqueeze(1)/255
         self.labels = torch.tensor(labels)
     
     def __getitem__(self,idx):
         return self.images[idx], self.labels[idx]
         
     def __len__(self):
-        print(len(images))
-        return len(self.images)
-    
+        #print(len(images))
+        return len(self.images) 
 
-BATCH_SIZE = 32
+BATCH_SIZE = 5
+mon_dataset = MonDataset(train_images,train_labels)
 
-train_dataloader = DataLoader(MonDataset(train_images), shuffle = True, batch_size = BATCH_SIZE)
-for batch, batch_len in train_dataloader:
-    print(batch, batch_len)
+# Creation of dataloader
+train_dataloader = DataLoader(mon_dataset, batch_size = BATCH_SIZE, shuffle = True)
+# batch_size : how many samples per batch to load (default is 1)
+# shuffle : set to 'True' to have the data reshuffled at every epoch (default is 'False')
+# sampler : defines the strategy to fraw samples from the dataset. Can be any Iterable with __len__ implemented~
+## if specified, shuffle must not be specified
+
